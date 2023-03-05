1742420b03714623ce0e8bf42b80f381cc418ecc
Archit Kandu <architkandu@hotmail.com>
2023-03-05 18:22:20 +0530

    Week-2 Flipping Bits

---
+++ 3_months_prep/Week-2/flipping_bits.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    num='{:032b}'.format(n)
+    inum=''
+    for i in num:
+        if i=='1':
+            inum+='0'
+        else:
+            inum+='1'
+    r=int(inum,2)
+    return r
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
